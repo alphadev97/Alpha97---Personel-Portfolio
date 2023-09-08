508e503206dcda783f16ca7175a6d02dd4eda740
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-08 23:52:41 +0500

    Static

--- static.js
+++ static.js
@@ -1,3 +1,5 @@
+// Header Navbar items
+
 export const navItems = [
   {
     title: "About",
@@ -17,7 +19,9 @@ export const navItems = [
   },
 ];
 
-export const socialIcons = [
+// Social Icons and Email
+
+export const socialData = [
   {
     title: "GitHub",
     url: "https://github.com/alphadev97",
@@ -33,5 +37,30 @@ export const socialIcons = [
   {
     title: "Instagram",
     url: "https://www.instagram.com/",
+  },
+  {
+    title: "dev095162@gmail.com",
+    url: "#",
+  },
+];
+
+// SECTIONS
+
+// Hero Section Data
+
+export const heroData = [
+  {
+    name: "Muhammad Usama",
+    Designation: "Software Engineer",
+  },
+];
+
+// About Section Data
+
+export const aboutData = [
+  {
+    title: "About",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit egestas ornare, nascetur non vehicula varius faucibus feugiat vestibulum pretium accumsan habitasse, nisi parturient viverra ullamcorper cras bibendum torquent morbi. Placerat ad morbi hac porttitor aliquet vehicula sem turpis, ullamcorper odio venenatis felis nisi vel cum egestas accumsan, massa quisque praesent id purus quis faucibus. Maecenas netus facilisi aliquam tellus diam dictumst ligula, facilisis magna platea auctor nulla habitasse per felis, habitant laoreet eros sagittis sociis accumsan.",
   },
 ];
