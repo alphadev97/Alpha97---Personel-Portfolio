74d9b5595196c8336dacc5a1c0aecda73b7f6705
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-08 23:46:21 +0500

    Static

--- static.js
+++ static.js
@@ -11,4 +11,27 @@ export const navItems = [
     title: "Projects",
     url: "/projects",
   },
+  {
+    title: "Contact",
+    url: "/contact",
+  },
+];
+
+export const socialIcons = [
+  {
+    title: "GitHub",
+    url: "https://github.com/alphadev97",
+  },
+  {
+    title: "Linkedin",
+    url: "https://www.linkedin.com/in/alpha97/",
+  },
+  {
+    title: "Facebook",
+    url: "https://www.facebook.com/",
+  },
+  {
+    title: "Instagram",
+    url: "https://www.instagram.com/",
+  },
 ];
