03847fc4a59e679bc93e8c722d22ae614691455c
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-27 22:00:23 +0500

    static

---
+++ src/data/static-data..ts
@@ -0,0 +1,320 @@
+// NAVIGATION ITEM
+export const navItems = [
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "About",
+    url: "#about",
+  },
+  {
+    id: 3,
+    title: "Projects",
+    url: "/projects",
+  },
+  {
+    id: 4,
+    title: "Skills",
+    url: "#skills",
+  },
+  {
+    id: 5,
+    title: "Experience",
+    url: "/experience",
+  },
+  {
+    id: 6,
+    title: "Contact",
+    url: "#contact",
+  },
+];
+
+// HERO ITEM
+export const heroItem = {
+  id: 1,
+  image_url: "/hero",
+  title: "hero",
+  name: "Hello, I'm Ricardo",
+  designation: "full-stack developer",
+  experience: "8 years",
+  focus: "React (Next.js).",
+  contact: {
+    title: "Contact me here",
+    url: "/",
+  },
+  cv: {
+    title: "Download CV",
+    url: "/",
+  },
+  iconOne: {
+    title: "GitHub",
+    image_url: "",
+    url: "/",
+  },
+  iconTwo: {
+    title: "Linkedin",
+    image_url: "",
+    url: "/",
+  },
+};
+
+// ABOUT ITEM
+export const aboutItem = {
+  title: "About Me",
+  description:
+    "After graduating with a degree in Accounting, I decided to pursue my passion for programming. I enrolled in a coding bootcamp and learned full-stack web development. My favorite part of programming is the problem-solving aspect. I love the feeling of finally figuring out a solution to a problem. My core stack is React, Next.js, Node.js, and MongoDB. I am also familiar with TypeScript and Prisma. I am always looking to learn new technologies. I am currently looking for a full-time position as a software developer.\n\nWhen I'm not coding, I enjoy playing video games, watching movies, and playing with my dog. I also enjoy learning new things. I am currently learning about history and philosophy. I'm also learning how to play the guitar.",
+};
+
+// PROJECTS ITEM
+export const projectsItem = [
+  {
+    title: "My Projects",
+    projects: [
+      {
+        id: 1,
+        title: "Lorem ipsum dolor sit amet",
+        image_url: "/",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, amet. Officiis optio quos sunt, unde id dolores quae dolor repudiandae consectetur aperiam, omnis sit ea labore suscipit, architecto iusto blanditiis!",
+        date: "20-2-23",
+        technology: [
+          {
+            id: 1,
+            title: "HTML",
+          },
+          {
+            id: 2,
+            title: "CSS",
+          },
+          {
+            id: 3,
+            title: "JAVASCRIPT",
+          },
+          {
+            id: 4,
+            title: "REACT.JS",
+          },
+          {
+            id: 5,
+            title: "NODE.JS",
+          },
+          {
+            id: 6,
+            title: "NEXT.JS",
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: "Lorem ipsum dolor sit amet",
+        image_url: "/",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, amet. Officiis optio quos sunt, unde id dolores quae dolor repudiandae consectetur aperiam, omnis sit ea labore suscipit, architecto iusto blanditiis!",
+        date: "20-2-23",
+        technology: [
+          {
+            id: 1,
+            title: "HTML",
+          },
+          {
+            id: 2,
+            title: "CSS",
+          },
+          {
+            id: 3,
+            title: "JAVASCRIPT",
+          },
+          {
+            id: 4,
+            title: "REACT.JS",
+          },
+          {
+            id: 5,
+            title: "NODE.JS",
+          },
+          {
+            id: 6,
+            title: "NEXT.JS",
+          },
+        ],
+      },
+      {
+        id: 3,
+        title: "Lorem ipsum dolor sit amet",
+        image_url: "/",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, amet. Officiis optio quos sunt, unde id dolores quae dolor repudiandae consectetur aperiam, omnis sit ea labore suscipit, architecto iusto blanditiis!",
+        date: "20-2-23",
+        technology: [
+          {
+            id: 1,
+            title: "HTML",
+          },
+          {
+            id: 2,
+            title: "CSS",
+          },
+          {
+            id: 3,
+            title: "JAVASCRIPT",
+          },
+          {
+            id: 4,
+            title: "REACT.JS",
+          },
+          {
+            id: 5,
+            title: "NODE.JS",
+          },
+          {
+            id: 6,
+            title: "NEXT.JS",
+          },
+        ],
+      },
+      {
+        id: 4,
+        title: "Lorem ipsum dolor sit amet",
+        image_url: "/",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, amet. Officiis optio quos sunt, unde id dolores quae dolor repudiandae consectetur aperiam, omnis sit ea labore suscipit, architecto iusto blanditiis!",
+        date: "20-2-23",
+        technology: [
+          {
+            id: 1,
+            title: "HTML",
+          },
+          {
+            id: 2,
+            title: "CSS",
+          },
+          {
+            id: 3,
+            title: "JAVASCRIPT",
+          },
+          {
+            id: 4,
+            title: "REACT.JS",
+          },
+          {
+            id: 5,
+            title: "NODE.JS",
+          },
+          {
+            id: 6,
+            title: "NEXT.JS",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+// SKILLS ITEM
+export const skillsItem = [
+  {
+    id: 1,
+    title: "HTML",
+  },
+  {
+    id: 2,
+    title: "CSS",
+  },
+  {
+    id: 3,
+    title: "JAVASCRIPT",
+  },
+  {
+    id: 4,
+    title: "REACT.JS",
+  },
+  {
+    id: 5,
+    title: "NODE.JS",
+  },
+  {
+    id: 6,
+    title: "EXPRESS.JS",
+  },
+  {
+    id: 7,
+    title: "TYPESCRIPT",
+  },
+];
+
+// EXPERIENCE ITEM
+export const experienceItem = [
+  {
+    id: 1,
+    company: "BlueTech International",
+    role: "Software Engineer",
+    from: "2018",
+    to: "",
+    description:
+      "Deliver high-quality, robust production code for a diverse array of projects for clients including Harvard Business School, Everytown for Gun Safety, Pratt Institute, Koala Health, Vanderbilt University, The 19th News, and more. Provide leadership within engineering department through close collaboration, knowledge shares, and mentorship",
+    stack: [
+      {
+        id: 1,
+        title: "HTML",
+      },
+      {
+        id: 2,
+        title: "CSS",
+      },
+      {
+        id: 3,
+        title: "JAVASCRIPT",
+      },
+    ],
+  },
+  {
+    id: 2,
+    company: "BlueTech International",
+    role: "Software Engineer",
+    from: "2018",
+    to: "",
+    description:
+      "Deliver high-quality, robust production code for a diverse array of projects for clients including Harvard Business School, Everytown for Gun Safety, Pratt Institute, Koala Health, Vanderbilt University, The 19th News, and more. Provide leadership within engineering department through close collaboration, knowledge shares, and mentorship",
+    stack: [
+      {
+        id: 1,
+        title: "HTML",
+      },
+      {
+        id: 2,
+        title: "CSS",
+      },
+      {
+        id: 3,
+        title: "JAVASCRIPT",
+      },
+    ],
+  },
+];
+
+// CONTACT ITEM
+export const contactItem = {
+  title: "Contact Me",
+  description:
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi beatae sunt, recusandae reiciendis repudiandae porro numquam neque illo, voluptas molestiae optio cumque tenetur, deleniti minus perspiciatis nemo natus voluptates harum?",
+  contact_items: [
+    {
+      id: 1,
+      title: "Email Address",
+      image_url: "",
+    },
+    {
+      id: 2,
+      title: "Phone No",
+      image_url: "",
+    },
+    {
+      id: 3,
+      title: "WhatsApp",
+      image_url: "",
+    },
+  ],
+};
