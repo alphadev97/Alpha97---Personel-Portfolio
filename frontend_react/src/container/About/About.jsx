8ed4a6d67ae05431c498f91911351ca1d208430e
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-27 21:48:03 +0500

    init

--- frontend_react/src/container/About/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./About.scss";
-
-const About = () => {
-  return <div>About</div>;
-};
-
-export default About;
--- frontend_react/src/container/Work/Work.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./Work.scss";
-
-export const Work = () => {
-  return <div>Work</div>;
-};
-
-export default Work;
