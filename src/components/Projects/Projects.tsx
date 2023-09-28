dc9d10938259d0a2cd954be193d62062561203e0
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-28 22:33:06 +0500

    Projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -7,23 +7,31 @@ const Projects = () => {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <h2>{projectsItem.title}</h2>
+        <h2 className={styles.title}>{projectsItem.title}</h2>
       </div>
 
       <div className={styles.projectCard}>
-        <Image src={"/temp.jpg"} width={600} height={500} alt="" />
+        <Image
+          className={styles.image}
+          src={"/temp.jpg"}
+          width={600}
+          height={450}
+          alt=""
+        />
         <div className={styles.projectContent}>
-          <h2>Lorem ipsum dolor sit amet consectetur</h2>
-          <p>
+          <h2 className={styles.projectTitle}>
+            Lorem ipsum dolor sit amet consectetur
+          </h2>
+          <p className={styles.projectDesc}>
             Lorem ipsum dolor sit amet consectetur, adipiscing elit semper
             tellus augue cubilia, nullam est conubia ornare. Pretium vulputate
             lectus nisl vitae ultrices eget sociosqu fringilla morbi euismod
             lacinia parturient,
           </p>
-          <h5>Date: 20.20.20</h5>
-          <h4>
-            Technology: WordPress, Timber, WordPress Multisite, Gutenberg,
-            JavaScript
+          <h5 className={styles.projectDate}>Date: 20.20.20</h5>
+          <h4 className={styles.projectTech}>
+            <b>Technology</b>: WordPress, Timber, WordPress Multisite,
+            Gutenberg, JavaScript
           </h4>
         </div>
       </div>
