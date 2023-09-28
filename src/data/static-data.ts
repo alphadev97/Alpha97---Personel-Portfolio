80a8c921a4b7f84eae2db6bc66d43d1b86c39c57
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-28 20:05:44 +0500

    About

--- src/data/static-data.ts
+++ src/data/static-data.ts
@@ -64,6 +64,7 @@ export const heroItem = {
 // ABOUT ITEM
 export const aboutItem = {
   title: "About Me",
+  url: "/",
   description:
     "After graduating with a degree in Accounting, I decided to pursue my passion for programming. I enrolled in a coding bootcamp and learned full-stack web development. My favorite part of programming is the problem-solving aspect. I love the feeling of finally figuring out a solution to a problem. My core stack is React, Next.js, Node.js, and MongoDB. I am also familiar with TypeScript and Prisma. I am always looking to learn new technologies. I am currently looking for a full-time position as a software developer.\n\nWhen I'm not coding, I enjoy playing video games, watching movies, and playing with my dog. I also enjoy learning new things. I am currently learning about history and philosophy. I'm also learning how to play the guitar.",
 };
