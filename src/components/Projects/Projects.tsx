6e2bbd239f998caef599638d6ac25db6e61dcf73
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-29 17:13:46 +0500

    Projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -2,38 +2,50 @@ import React from "react";
 import styles from "./projects.module.css";
 import { projectsItem } from "@/data/static-data";
 import Image from "next/image";
+import Link from "next/link";
 
 const Projects = () => {
+  // Sort projects by date in descending order (latest first)
+  const sortedProjects = projectsItem.projects
+    .slice()
+    .sort((a, b) => b.date.getTime() - a.date.getTime());
+
+  // Select the first 3 projects (latest 3)
+  const latestProjects = sortedProjects.slice(0, 3);
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         <h2 className={styles.title}>{projectsItem.title}</h2>
       </div>
 
-      <div className={styles.projectCard}>
-        <Image
-          className={styles.image}
-          src={"/temp.jpg"}
-          width={600}
-          height={450}
-          alt=""
-        />
-        <div className={styles.projectContent}>
-          <h2 className={styles.projectTitle}>
-            Lorem ipsum dolor sit amet consectetur
-          </h2>
-          <p className={styles.projectDesc}>
-            Lorem ipsum dolor sit amet consectetur, adipiscing elit semper
-            tellus augue cubilia, nullam est conubia ornare. Pretium vulputate
-            lectus nisl vitae ultrices eget sociosqu fringilla morbi euismod
-            lacinia parturient,
-          </p>
-          <h5 className={styles.projectDate}>Date: 20.20.20</h5>
-          <h4 className={styles.projectTech}>
-            <b>Technology</b>: WordPress, Timber, WordPress Multisite,
-            Gutenberg, JavaScript
-          </h4>
+      {latestProjects.map((project) => (
+        <div key={project.id} className={styles.projectCard}>
+          <Image
+            className={styles.image}
+            src={project.image_url}
+            width={600}
+            height={450}
+            alt=""
+          />
+          <div className={styles.projectContent}>
+            <h2 className={styles.projectTitle}>{project.title}</h2>
+            <p className={styles.projectDesc}>{project.description}</p>
+            <h5 className={styles.projectDate}>
+              Date: {project.date.toLocaleDateString()}
+            </h5>
+            <h4 className={styles.projectTech}>
+              <b>Technology</b>:{" "}
+              {project.technology.map((tech) => tech.title).join(", ")}
+            </h4>
+          </div>
         </div>
+      ))}
+
+      <div className={styles.button}>
+        <Link className={styles.btn} href={projectsItem.url}>
+          {projectsItem.btnTitle}
+        </Link>
       </div>
     </div>
   );
