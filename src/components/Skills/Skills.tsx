de786008a1a34e1c0b492fa59f972ba43b36b2c5
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-02 23:12:20 +0500

    Skills

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -3,7 +3,7 @@ import styles from "./skills.module.css";
 
 const Skills = () => {
   return (
-    <div className={styles.container}>
+    <div id="skills" className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>My Skills</h1>
 
