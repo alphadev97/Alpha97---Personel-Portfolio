22a2caee21fc016a3f277facb4824a4f789f386f
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-03 18:29:33 +0500

    Theme

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -8,8 +8,6 @@ import { ThemeContext } from "@/context/ThemeContext";
 const ThemeToggle = () => {
   const { toggle, theme } = useContext(ThemeContext)!;
 
-  console.log(theme);
-
   return (
     <div
       className={styles.container}
