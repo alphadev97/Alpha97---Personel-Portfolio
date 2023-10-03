d5a5b616d0fb1388bff9f04a2cf8b87cc741b6e8
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-03 17:21:28 +0500

    toast

--- src/components/Form/sendForm.tsx
+++ src/components/Form/sendForm.tsx
@@ -1,11 +1,24 @@
 "use client";
 
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import styles from "./form.module.css";
+import { toast } from "react-toastify";
 
 export const ContactUs = () => {
   const form = useRef<HTMLFormElement | null>(null);
+  const [formData, setFormData] = useState({
+    from_name: "",
+    email_id: "",
+    message: "",
+  });
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
   const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -20,10 +33,16 @@ export const ContactUs = () => {
         )
         .then(
           (result) => {
+            toast.success("Form Submitted Successfully!");
             console.log(result.text);
-            console.log(form);
+            setFormData({
+              from_name: "",
+              email_id: "",
+              message: "",
+            });
           },
           (error) => {
+            toast.error("Something Went Wrong!");
             console.log(error.text);
           }
         );
@@ -33,15 +52,29 @@ export const ContactUs = () => {
   return (
     <div className={styles.send}>
       <form ref={form} onSubmit={sendEmail} className={styles.form}>
-        <input type="text" name="from_name" placeholder="Your Name" />
+        <input
+          type="text"
+          name="from_name"
+          placeholder="Your Name"
+          value={formData.from_name}
+          onChange={handleInputChange}
+        />
 
-        <input type="email" name="email_id" placeholder="Your Email Address" />
+        <input
+          type="email"
+          name="email_id"
+          placeholder="Your Email Address"
+          value={formData.email_id}
+          onChange={handleInputChange}
+        />
 
         <textarea
           name="message"
           cols={30}
           rows={10}
           placeholder="Your Message"
+          value={formData.message}
+          onChange={handleInputChange}
         />
         <input type="submit" value="Send" className={styles.submitBtn} />
       </form>
